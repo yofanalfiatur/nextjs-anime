0b4975847b4c6bfc629f9dd4577a20162725f9d4
yofanalfiatur <60646990+yofanalfiatur@users.noreply.github.com>
2023-12-26 15:46:37 +0700

    kondisi pagin

--- src/components/Utilities/Pagination/index.jsx
+++ src/components/Utilities/Pagination/index.jsx
@@ -18,22 +18,27 @@ const Pagination = (props) => {
     scrollTop();
   };
   const handlePreviousPage = () => {
-    if (page === 1) return;
     setPage((prevState) => prevState - 1);
     scrollTop();
   };
   return (
     <>
       <div className="flex flex-row justify-center items-center gap-4 w-full py-10">
-        <button onClick={handlePreviousPage}>
-          <CaretDoubleLeft size={32} color="#e0e0e0" />
-        </button>
+        {page <= 1 ? null : (
+          <button onClick={handlePreviousPage}>
+            <CaretDoubleLeft size={32} color="#e0e0e0" />
+          </button>
+        )}
+
         <p>
           {page} of {lastPage}{" "}
         </p>
-        <button onClick={handleNextPage}>
-          <CaretDoubleRight size={32} color="#e0e0e0" />
-        </button>
+
+        {page >= lastPage ? null : (
+          <button onClick={handleNextPage}>
+            <CaretDoubleRight size={32} color="#e0e0e0" />
+          </button>
+        )}
       </div>
     </>
   );
